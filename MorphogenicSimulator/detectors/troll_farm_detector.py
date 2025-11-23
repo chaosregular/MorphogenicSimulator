b1619f388227d0d2d49e6945106c6a667678d2cb
ChaosRegular <chaosregular@gmail.com>
2025-11-23 01:06:49 +0100

    Update troll_farm_detector.py
    from other output same exchange - added from other output window in https://github.com/chaosregular/MorphogenicSimulator/wiki/zzz_Seek_2025_11_23_00_29_43.md

--- MorphogenicSimulator/detectors/troll_farm_detector.py
+++ MorphogenicSimulator/detectors/troll_farm_detector.py
@@ -10,7 +10,25 @@ class TrollFarmDetector:
         self.dissonance_detector = BasicDissonanceDetector(resonance_map_path)
         self.graph_builder = MorphogenicGraphBuilder(resonance_map_path)
         self.user_profiles = defaultdict(dict)
-    
+
+# from other output same exchange
+    def detect_synchronized_behavior(self, messages, time_window):
+        # Farmy trolli mają:
+        # - Wysoką koherencję wewnętrzną
+        # - Niski dysonans z dark attractors  
+        # - Synchronizację czasową odpowiedzi
+        # - Niską variance w embeddingach
+        
+        coherence = self.calculate_semantic_coherence(messages)
+        time_sync = self.analyze_response_timing(messages, time_window)
+        dark_affinity = self.measure_dark_attractor_affinity(messages)
+        
+        troll_score = (coherence * 0.3 + 
+                      time_sync * 0.4 + 
+                      dark_affinity * 0.3)
+        
+        return troll_score > 0.7    
+# /from other output same exchange
     def analyze_conversation_thread(self, messages, time_window_hours=24):
         """Analizuje wątek dyskusji pod kątem zsynchronizowanych zachowań"""
         # Implementacja detekcji farm trolli
