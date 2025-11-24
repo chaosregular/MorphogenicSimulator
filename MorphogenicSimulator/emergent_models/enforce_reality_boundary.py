18e391f2e7430312c4ed14f5681d6bfef6435f18
ChaosRegular <chaosregular@gmail.com>
2025-11-24 19:45:30 +0100

    Update enforce_reality_boundary.py
    Seek update from https://github.com/chaosregular/MorphogenicSimulator/wiki/zzz_Seek_2025_11_23_01_47_31.md

--- MorphogenicSimulator/emergent_models/enforce_reality_boundary.py
+++ MorphogenicSimulator/emergent_models/enforce_reality_boundary.py
@@ -1,4 +1,19 @@
 # W kodzie immunologicznego frameworku
+def personal_evil_containment(threat, defender):
+    """
+    Zasada osobistego odprowadzenia zła do piekła:
+    - Gotowość do destrukcji obu stron konfliktu
+    - Naturalny hamulec eskalacji
+    - Zapobiega infinite loops przemocy
+    """
+    containment_cost = defender.calculate_sacrifice_cost(threat)
+    
+    if containment_cost > defender.existence_threshold:
+        # Przekroczenie progu - interwencja niszczy obie strony
+        return MutualAnnihilationProtocol(defender, threat)
+    else:
+        return StandardContainment(defender, threat)
+        
 def enforce_reality_boundary(threat, enforcer):
     """
     Wymuszenie musi być:
